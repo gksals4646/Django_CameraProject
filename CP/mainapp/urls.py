65b9ff40c6106cd5654439d48d03b60c16d4808f
sidewing26 <63769551+sidewing26@users.noreply.github.com>
2020-08-11 19:07:52 +0900

    상품페이지

--- CP/mainapp/urls.py
+++ CP/mainapp/urls.py
@@ -4,8 +4,9 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('item/', views.item, name='item'),
-    # path('rank/', views.rank, name='rank'),
-    # path('album/', views.album, name='album'),
+    path('', views.index, name='index'),
+    path('item_body/', views.item_body, name='item_body'),
+    path('item_lens/', views.item_lens, name='item_lens'),
+    path('rank/', views.rank, name='rank'),
+    path('album/', views.album, name='album'),
 ]
--- CP/mainapp/views.py
+++ CP/mainapp/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+# 메인 페이지
+def index(request):
+    return render(request, 'index.html')
+
+# 상품 페이지
+def item_body(request):
+    product_body = Product.objects.filter(bodytype='1')
+    return render(request, 'item_body.html', {'product_body':product_body})
+
+def item_lens(request):
+    product_lens = Product.objects.filter(bodytype='2')
+    return render(request, 'item_lens.html', {'product_lens':product_lens})
+
+
+# 랭킹 페이지
+def rank(request):
+    return render(request, 'rank.html')
+
+# 사진첩 페이지
+def album(request):
+    return render(request, 'album.html')
