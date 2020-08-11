4aa7109cc29df835aa832def0cf9376a575cc904
leesg619 <52132929+leesg619@users.noreply.github.com>
2020-08-11 15:05:19 +0900

    template추가

--- CP/mainapp/urls.py
+++ CP/mainapp/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    # path('', admin.site.urls),
+    path('', views.index, name='index'),
+    path('item/', views.item, name='item'),
+    path('rank/', views.rank, name='rank'),
+    path('album/', views.album, name='album'),
 ]
