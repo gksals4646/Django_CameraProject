143b70426a041279e96803aa03f4793e258708fa
sidewing26 <63769551+sidewing26@users.noreply.github.com>
2020-08-14 15:08:17 +0900

    ㄴㄴ

--- CP/mainapp/urls.py
+++ CP/mainapp/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     # path('item/', views.item, name='item'),
     # path('rank/', views.rank, name='rank'),
     # path('album/', views.album, name='album'),
-    #path('item_detail/', views.item_detail, name = 'item_detail'),
+    # path('item_detail/', views.item_detail, name = 'item_detail'),
 ]
--- CP/pdapp/models.py
+++ CP/pdapp/models.py
@@ -7,8 +7,8 @@ class LensType(models.Model):
     lname=models.CharField(max_length=200, null=True)
 
 class BodyType(models.Model):
-    filmb=models.CharField(max_length=200, null=True) #필름 혹은 디지털 타입인데, 후에 이것을 bodyname으로 통합할것.(필름디지털삭제할것)
-    digitalb=models.CharField(max_length=200, null=True) #임의로 bodyname대신  filmb를 사용하겠음
+    filmb=models.CharField(max_length=200, null=True) # 필름 혹은 디지털 타입인데, 후에 이것을 bodyname으로 통합할것.(필름디지털삭제할것)
+    digitalb=models.CharField(max_length=200, null=True) # 임의로 bodyname대신  filmb를 사용하겠음
 
 class Brand(models.Model):
     bname = models.CharField(max_length=50, null=True)
@@ -19,8 +19,5 @@ class Product(models.Model):
     price = models.IntegerField(null = True)
     pic = models.ImageField(null = True, upload_to="%Y/%m/%d")
     star = models.IntegerField(null = True) #별점
-    bodytype = models.ForeignKey(BodyType, null = True , on_delete = models.CASCADE)
-    lenstype = models.ForeignKey(LensType, null = True , on_delete = models.CASCADE)
-
-
-
+    bodytype = models.ForeignKey(BodyType, null = True , blank = True , on_delete = models.CASCADE)
+    lenstype = models.ForeignKey(LensType, null = True , blank = True , on_delete = models.CASCADE)
