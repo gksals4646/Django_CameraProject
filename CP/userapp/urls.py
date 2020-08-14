6daadd298a8db3da485d0132c8a1403641330818
bingsuya <qls2540@naver.com>
2020-08-14 15:14:58 +0900

    메뉴에서 로그인페이지, 마이페이지 연결
    메뉴바 마이페이지에서 밑으로 쭈르륵 뜨는거 없애고 마이페이지로 가게해놨음

--- CP/userapp/urls.py
+++ CP/userapp/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path('signup_done/', views.signup_done, name = 'signup_done'),
     path('logout/', views.logout, name = 'logout'),
     path('mypage/', views.mypage, name = 'mypage'),
-    
-    
+    '''path('mypic/', views.mypic, name = 'mypic'),
+    path('myreview/', views.myreview, name = 'myreview'),
+    path('myitem/', views.myitem, name = 'myitem'),
+    path('myinfo/', views.myinfo, name = 'myinfo'),'''
 ]
