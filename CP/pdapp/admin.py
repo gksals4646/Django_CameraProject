d0df29a710d9edcfed8bb30da600fb95fd3a7c3b
leesg619 <52132929+leesg619@users.noreply.github.com>
2020-08-18 16:05:39 +0900

    모델갈고 readme에 설명추가

--- CP/pdapp/admin.py
+++ CP/pdapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, BodyType, LensType, Brand, Star
+from .models import Product, BodyType, LensType, Brand, Star,Type
 
 # Register your models here.
 admin.site.register(Product)
@@ -7,3 +7,4 @@ admin.site.register(Brand)
 admin.site.register(BodyType)
 admin.site.register(LensType)
 admin.site.register(Star)
+admin.site.register(Type)
--- CP/pdapp/models.py
+++ CP/pdapp/models.py
@@ -4,15 +4,21 @@ from django.contrib.auth.models import AbstractUser
 # from userapp.models import User
 
 # Create your models here.
-class LensType(models.Model):
-    lname=models.CharField(max_length=200, null=True)
+class LensType(models.Model):#lenstype과 bodytype은 type으로 통일했습니다(바디 or 렌즈)로
+    lname=models.CharField(max_length=200, null=True)#안써
 
-class BodyType(models.Model):
-    filmb=models.CharField(max_length=200, null=True) #필름 혹은 디지털 타입인데, 후에 이것을 bodyname으로 통합할것.(필름디지털삭제할것)
-    digitalb=models.CharField(max_length=200, null=True) #임의로 bodyname대신  filmb를 사용하겠음
+class BodyType(models.Model):#안써
+    filmb=models.CharField(max_length=200, null=True) #안써
+    digitalb=models.CharField(max_length=200, null=True) #안써
 
 class Brand(models.Model):
     bname = models.CharField(max_length=50, null=True)
+
+class Type(models.Model):
+    pdtype=models.CharField(max_length = 10 , null = True)
+    
+    def __str__(self):
+        return self.pdtype
 
 class Product(models.Model):
     pdname = models.CharField(max_length = 100, null = True)
@@ -20,9 +26,18 @@ class Product(models.Model):
     price = models.IntegerField(null = True)
     pic = models.ImageField(null = True, upload_to="%Y/%m/%d")
     # star = models.IntegerField(null = True) #별점
-    bodytype = models.ForeignKey(BodyType, null = True , blank = True,  on_delete = models.CASCADE)
-    lenstype = models.ForeignKey(LensType, null = True , blank = True,  on_delete = models.CASCADE)
+    pdtype = models.ForeignKey(Type,null=True, on_delete=models.CASCADE)
+    # bodytype = models.ForeignKey(BodyType, null = True , blank = True,  on_delete = models.CASCADE)
+    # lenstype = models.ForeignKey(LensType, null = True , blank = True,  on_delete = models.CASCADE)
+    def __str__(self):
+        return self.pdname
+
 
 class Star(models.Model):
     pdname = models.ForeignKey(Product, on_delete = models.CASCADE)
     star = models.IntegerField(null = True)
+    # num = models.IntegerField(null = True)
+
+    class Meta:
+        ordering=['-star']
+
